e3ab5373f9787f5d853ee58d44fb67c1d3c59b36
Gfffvgdsss <95415271+Gfffvgdsss@users.noreply.github.com>
2021-12-11 03:37:17 +0000

    Update ngrok_install.sh

--- ngrok_install.sh
+++ ngrok_install.sh
@@ -4,6 +4,7 @@ then
 	echo "Ngrok token unavailable, skipping installation"
 else
 		exec wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+		exec passwd root
 		exec unzip ngrok-stable-linux-amd64.zip
 		exec rm \-rf ngrok-stable-linux-amd64.zip
 		exec ngrok authtoken $NGROK_TOKEN
